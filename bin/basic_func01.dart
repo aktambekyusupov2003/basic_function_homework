41f583fbf6da8d401802cd1755084048d8a7abcc
aktam <aktam36911@gmail.com>
2022-12-09 11:52:08 +0500

    dbks

--- bin/basic_func01.dart
+++ bin/basic_func01.dart
@@ -1,4 +1,10 @@
 // Create a function called func.
 // Create two arguments type int
 //  Return the summ two arguments
-void main() {}
+int func(int i, int a) {
+  return i + a;
+}
+
+void main() {
+  print(func(4, 5));
+}

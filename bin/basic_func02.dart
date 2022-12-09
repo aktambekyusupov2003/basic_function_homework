cda1cca390f73b91aea94ffa0d3bf8fdc7b9c137
aktam <aktam36911@gmail.com>
2022-12-09 22:59:33 +0500

    bsic func02

--- bin/basic_func02.dart
+++ bin/basic_func02.dart
@@ -1,4 +1,11 @@
-// Create a function called divide. 
-//Create function arguments a and b of type int. 
+// Create a function called divide.
+//Create function arguments a and b of type int.
 //Returns the floor division of a and b.
-void main() {}
+
+num func(int b, int d) {
+  return (b / d);
+}
+
+void main() {
+  print(func(8, 2));
+}
